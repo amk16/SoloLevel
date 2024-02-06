20187ca3fd4fdd0658b49b92fb63a605a015cbcc
Ali <alimalik16@hotmail.com>
2024-02-06 15:13:33 +0000

    Extend system class

--- system.py
+++ system.py
@@ -1,4 +1,5 @@
 from user_profile import UserProfile
+from task import Task
 class system():
     def __init__(self):
         self.user_profile = UserProfile()
@@ -9,3 +10,30 @@ class system():
         if hasattr(self,command) and callable(getattr(self,command)):
             method = getattr(self,command)
             method(*args)
+
+    def add_task(self,title,Id,exp,reward,complete_reqs):
+        
+        task = Task(title=title,Id=Id,exp=exp,reward=reward,complete_reqs=complete_reqs)
+        self.user_profile.get_active_tasks().append(task)
+        print(f"Task {Id}: {title} has been created")
+
+    def complete_task(self,Id):
+        for task in self.user_profile.get_active_tasks():
+            if task.Id == Id:
+                self.user_profile.finished_tasks.append(task)
+                self.user_profile.active_tasks.remove(task)
+                print(f"Task {task.getId()}: {task.getTitle()} has been completed")
+                return
+            else:
+                print(f"Task {Id} does not exist")
+
+    def show_tasks(self):
+        print("Active tasks:")
+        for task in self.user_profile.get_active_tasks():
+            print(f"- {task.getId()}: {task.getTitle()}")
+        
+        print("Finished tasks:")
+        for task in self.user_profile.get_active_tasks():
+            print(f"- {task.getId()}: {task.getTitle()}")
+
+    
--- task.py
+++ task.py
@@ -15,18 +15,24 @@ class Task():
             "reward" : self.reward,
             "complete_reqs":  self.complete_reqs,
         }
-    
+    @classmethod
     def from_dict(cls,data):
         return cls(title=data["title"], Id = data["Id"], exp = data["exp"], reward = data["reward"], complete_reqs = data["complete_reqs"])
 
     def edit_title(self, title):
         self.title = title
+
+    def edit_Id(self,Id):
+        self.Id = Id
     
     def edit_exp(self,exp):
         self.exp = exp
 
     def edit_reward(self,reward):
         self.reward = reward
+    
+    def edit_complete_reqs(self, complete_reqs):
+        self.complete_reqs = complete_reqs
 
 
     
